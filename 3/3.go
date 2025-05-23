adb5f60e3e69591aebc77ea6e3e3d1ba4e27c644
unknown <nasaka9512@gmail.com>
2025-05-22 20:10:22 -0700

    first solve - bad

--- 3/3.go
+++ 3/3.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
+
+func simpleSolution(number int) int {
+	var max = int(math.Sqrt(float64(number)))
+	var factor = 2
+	var result = 0
+	for factor < max {
+		if number%factor == 0 {
+			result = number
+			number = number / factor
+			if number == 0 {
+				break
+			}
+			factor = 2
+			continue
+		}
+		factor++
+	}
+	return result
+}
 
 func main() {
+	var number = 600851475143
+	result := simpleSolution(number)
+	print(result)
 }
