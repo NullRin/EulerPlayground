6e5fb9c6cc527d1220288654ec471332112454c9
unknown <NullRin@users.noreply.github.com>
2025-06-25 16:00:36 -0700

    fix initial factor count to 2.

--- 12/12.go
+++ 12/12.go
@@ -6,16 +6,17 @@ import (
 )
 
 func main() {
+	var factorGoal int16 = 500
 	var triangleNumber = 0
 	var i = 0
 	var factor = 1
 	var triangleRoot int
 	var factorCount int16
-	for factorCount < 500 {
+	for factorCount < factorGoal {
 		i++
 		triangleNumber += i
 		triangleRoot = int(math.Sqrt(float64(triangleNumber)))
-		factorCount = 0
+		factorCount = 2
 		factor = 2
 		for factor <= triangleRoot {
 			if triangleNumber%factor == 0 {
