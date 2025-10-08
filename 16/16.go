10d5ed3c713b22e618baa1eaebcc781f9a6962de
NullRin <nasaka9512@gmail.com>
2025-10-08 16:26:51 -0700

    16 solution

---
+++ 16/16.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	// manual multiplication
+	// 2^1000 has about 300 digits
+	var length uint16 = 350
+	var sum = make([]uint8, length)
+	var i uint16 = 1
+	var j uint16 = 1
+	var temp uint8
+	var carry uint8
+	var totalSum uint32 = 0
+	sum[0] = 2
+	for i < 1000 {
+		j = 0
+		temp = 0
+		carry = 0
+		for j < length-1 {
+			temp = sum[j] << 1
+			if temp >= 10 {
+				temp = temp - 10
+				sum[j] = temp + carry
+				carry = 1
+			} else {
+				sum[j] = temp + carry
+				carry = 0
+			}
+			j++
+		}
+		i++
+	}
+	i = 0
+	for i < length {
+		totalSum += uint32(sum[i])
+		i++
+
+	}
+	fmt.Println(sum)
+	fmt.Println(totalSum)
+}
